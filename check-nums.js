5197d67d36c9e463b5ff549369539377bbb528b1
Mafoa <steve_mafoa@outlook.com>
2023-02-02 23:37:28 +0100

    check-nums

---
+++ check-nums.js
@@ -0,0 +1,12 @@
+ let num1 = 120
+ let num2 = 12
+
+if (num2 > num1) {
+    console.log('num 2 est suprérieur à num1')
+}
+else if(num1 == num2){
+    console.log('num1 et num2 sont equivalents')
+}
+else{
+    console.log('num2 est inférieur à num1')
+}
