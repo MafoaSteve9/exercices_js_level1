a90e3ad568544467c7b263f2275ca834f09897e2
Mafoa <steve_mafoa@outlook.com>
2023-02-06 15:36:53 +0100

    Number

---
+++ Number.js
@@ -0,0 +1,37 @@
+let v1 = 50, v2 = 100, v3 = 150, v4 = 200, v5 = 2, v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+      return a+b
+}
+equal1()
+
+
+ function equal2(){
+   let a = v3
+       b = v1
+   return a - b
+ }
+ equal2()
+
+ function equal3(){
+   let a = v5
+       b = v1
+   return a * b
+ }
+ equal3()
+
+ function equal4(){
+   let a = v2
+       b = v4
+   return a / b;
+ }
+ equal4()
+
+ function equal5(){
+   let a = v6
+       b = v3
+   return a % b
+ }
+ equal5()
