98d26ed078976444ac0fb21f58505487f5d9e4fd
Mafoa <steve_mafoa@outlook.com>
2023-02-07 22:48:41 +0100

    button

--- recette.js
+++ recette.js
@@ -1,8 +1,11 @@
 let p = document.querySelector('p').innerHTML = "Il faudra sept ingrédients pour réalisé la recette carbonara :"
-
+let button = document.querySelector('button')
 const ingredients = ["", "Pâtes", "Oeuf", "Sel", "Poivre", "Lardon", "Oignon", "Parmesan"].sort()
-for(i = 1; i < ingredients.length; i++){
-    
-    document.write(`<br>${i}. ${ingredients[i]}`)
-}
 
+button.addEventListener('click', () => {
+    
+    for(i = 1; i < ingredients.length; i++){
+    
+        document.write(`<br>${i}. ${ingredients[i]}`)
+    }
+})
